e6bb05c8281da2d0407a95a4d704e9613c0c844a
David Zhang <ruiqing.zhang18@gmail.com>
2017-04-01 02:21:48 -0400

    Updating

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ var https = require('https');
 var queryString = require('querystring');
 
 // Lambda function:
-exports.handler = function (event, context) {
+// exports.handler = function (event, context) {
 
-    console.log('Running event');
+//     console.log('Running event');
     
-    // Send an SMS message to the number provided in the event data.
-    // End the lambda function when the send function completes.
-    SendSMS(event.to, 'Hello from Lambda Functions!', 
-                function (status) { context.done(null, status); });  
-};
+//     // Send an SMS message to the number provided in the event data.
+//     // End the lambda function when the send function completes.
+//     SendSMS(event.to, 'Hello from Lambda Functions!', 
+//                 function (status) { context.done(null, status); });  
+// };
 
 // Sends an SMS message using the Twilio API
 // to: Phone number to send to
@@ -125,44 +125,43 @@ function SendSMS(to, body, callback) {
 // /*============================================================*/
 // Route the incoming request based on type (LaunchRequest, IntentRequest,
 // etc.) The JSON body of the request is provided in the event parameter.
-exports.handler = function (event, context) {
-    try {
-        console.log("event.session.application.applicationId=" + event.session.application.applicationId);
+// exports.handler = function (event, context) {
+//     try {
+//         console.log("event.session.application.applicationId=" + event.session.application.applicationId);
 
-        /**
-         * Uncomment this if statement and populate with your skill's application ID to
-         * prevent someone else from configuring a skill that sends requests to this function.
-         */
-        /*
-        if (event.session.application.applicationId !== "amzn1.echo-sdk-ams.app.[unique-value-here]") {
-             context.fail("Invalid Application ID");
-         }
-        */
+//         /**
+//          * Uncomment this if statement and populate with your skill's application ID to
+//          * prevent someone else from configuring a skill that sends requests to this function.
+//          */
+//         /*
+//         if (event.session.application.applicationId !== "amzn1.echo-sdk-ams.app.[unique-value-here]") {
+//              context.fail("Invalid Application ID");
+//          }
+//         */
+//         if (event.session.new) {
+//             onSessionStarted({requestId: event.request.requestId}, event.session);
+//         }
 
-        if (event.session.new) {
-            onSessionStarted({requestId: event.request.requestId}, event.session);
-        }
-
-        if (event.request.type === "LaunchRequest") {
-            onLaunch(event.request,
-                     event.session,
-                     function callback(sessionAttributes, speechletResponse) {
-                        context.succeed(buildResponse(sessionAttributes, speechletResponse));
-                     });
-        }  else if (event.request.type === "IntentRequest") {
-            onIntent(event.request,
-                     event.session,
-                     function callback(sessionAttributes, speechletResponse) {
-                         context.succeed(buildResponse(sessionAttributes, speechletResponse));
-                     });
-        } else if (event.request.type === "SessionEndedRequest") {
-            onSessionEnded(event.request, event.session);
-            context.succeed();
-        }
-    } catch (e) {
-        context.fail("Exception: " + e);
-    }
-};
+//         if (event.request.type === "LaunchRequest") {
+//             onLaunch(event.request,
+//                      event.session,
+//                      function callback(sessionAttributes, speechletResponse) {
+//                         context.succeed(buildResponse(sessionAttributes, speechletResponse));
+//                      });
+//         }  else if (event.request.type === "IntentRequest") {
+//             onIntent(event.request,
+//                      event.session,
+//                      function callback(sessionAttributes, speechletResponse) {
+//                          context.succeed(buildResponse(sessionAttributes, speechletResponse));
+//                      });
+//         } else if (event.request.type === "SessionEndedRequest") {
+//             onSessionEnded(event.request, event.session);
+//             context.succeed();
+//         }
+//     } catch (e) {
+//         context.fail("Exception: " + e);
+//     }
+// };
 
 var Alexa = require('alexa-sdk');
 
@@ -177,7 +176,30 @@ var Alexa = require('alexa-sdk');
 
         var handlers = {
             'LaunchRequest': function () {
-                this.emit('MyIntent');
+                this.emit(':tell', 'Welcome to Scheduler-Pro. Please state your tasks today and their priority from 1 to 10');
+            },
+            'SendSMS': function(){
+
+                var intent = this.event.request.intent,
+                    intentName = this.event.request.intent.name;
+                if("SendSMS" === intentName){
+                    var destination = this.event.request.intent.slots.Destination.value;
+                    var text = this.event.request.intent.slots.Text.value;
+                    var number;
+                    if("David" === destination){
+                        number = "5197217737";
+                    } else if ("david" === destination){
+                        number = "5197217737";
+                    } else if ("bob" === destination){
+                        number = "5197217737";
+                    }
+                        SendSMS(number ,text,callback);
+                    } else {
+                        throw "Invalid intent";
+                    }
+                }
+
+                var myName = this.event.request.intent.slots.firstname.value;
             },
             'AMAZON.HelpIntent': function(){
                 this.emit(':ask', 'Do you need help');
@@ -198,89 +220,111 @@ var Alexa = require('alexa-sdk');
 /**
  * Called when the session starts.
  */
-function onSessionStarted(sessionStartedRequest, session) {
-    console.log("onSessionStarted requestId=" + sessionStartedRequest.requestId +
-            ", sessionId=" + session.sessionId);
-}
+// function onSessionStarted(sessionStartedRequest, session) {
+//     console.log("onSessionStarted requestId=" + sessionStartedRequest.requestId +
+//             ", sessionId=" + session.sessionId);
+// }
 
-/**
- * Called when the user launches the skill without specifying what they want.
- */
-function onLaunch(launchRequest, session, callback) {
-    console.log("onLaunch requestId=" + launchRequest.requestId +
-            ", sessionId=" + session.sessionId);
+// *
+//  * Called when the user launches the skill without specifying what they want.
+ 
+// function onLaunch(launchRequest, session, callback) {
+//     console.log("onLaunch requestId=" + launchRequest.requestId +
+//             ", sessionId=" + session.sessionId);
 
-    // Dispatch to your skill's launch.
-    getWelcomeResponse(callback);
+//     // Dispatch to your skill's launch.
+//     getWelcomeResponse(callback);
    
   
-}
+// }
 
+// function onIntent(intentRequest, session, callback) {
+//     console.log("onIntent requestId=" + intentRequest.requestId +
+//             ", sessionId=" + session.sessionId);
 
+//     var intent = intentRequest.intent,
+//         intentName = intentRequest.intent.name;
+//   if("SendSMS" === intentName){
+//         var destination = intentRequest.intent.slots.Destination.value;
+//         var text = intentRequest.intent.slots.Text.value;
+//         var number;
+//         if("David" === destination){
+//             number = "5197217737";
+//         } else if ("david" === destination){
+//             number = "5197217737";
+//         } else if ("bob" === destination){
+//             number = "5197217737";
+//         }
+//         SendSMS(number ,text,callback);
+//     } else {
+//         throw "Invalid intent";
+//     }
+    
+// }
 
 /**
  * Called when the user specifies an intent for this skill.
  */
-function onIntent(intentRequest, session, callback) {
-    console.log("onIntent requestId=" + intentRequest.requestId +
-            ", sessionId=" + session.sessionId);
+// function onIntent(intentRequest, session, callback) {
+//     console.log("onIntent requestId=" + intentRequest.requestId +
+//             ", sessionId=" + session.sessionId);
 
-    var intent = intentRequest.intent,
-        intentName = intentRequest.intent.name;
-/*==============================================*/
-    if("userList" === intentName){
-        var text = IntentRequest.intent.slots.tasks.value;
-        var textString = text.toString();
+//     var intent = intentRequest.intent,
+//         intentName = intentRequest.intent.name;
+// /*==============================================*/
+//     if("userList" === intentName){
+//         var text = IntentRequest.intent.slots.tasks.value;
+//         var textString = text.toString();
 
-        responseTool(textString,callback);
-    }
-/*==============================================*/
-   else if("SendSMS" === intentName){
-        var destination = intentRequest.intent.slots.Destination.value;
-        var text = intentRequest.intent.slots.Text.value;
-        var number;
-        if("David" === destination){
-            number = "5197217737";
-        } else if ("david" === destination){
-            number = "5197217737";
-        } else if ("bob" === destination){
-            number = "5197217737";
-        }
-        SendSMS(number ,text,callback);
-    } else {
-        throw "Invalid intent";
-    }
+//         responseTool(textString,callback);
+//     }
+// ==============================================
+//    else if("SendSMS" === intentName){
+//         var destination = intentRequest.intent.slots.Destination.value;
+//         var text = intentRequest.intent.slots.Text.value;
+//         var number;
+//         if("David" === destination){
+//             number = "5197217737";
+//         } else if ("david" === destination){
+//             number = "5197217737";
+//         } else if ("bob" === destination){
+//             number = "5197217737";
+//         }
+//         SendSMS(number ,text,callback);
+//     } else {
+//         throw "Invalid intent";
+//     }
     
-}
+// }
 
 /**
  * Called when the user ends the session.
  * Is not called when the skill returns shouldEndSession=true.
  */
-function onSessionEnded(sessionEndedRequest, session) {
-    console.log("onSessionEnded requestId=" + sessionEndedRequest.requestId +
-            ", sessionId=" + session.sessionId);
-    // Add cleanup logic here
-}
+// function onSessionEnded(sessionEndedRequest, session) {
+//     console.log("onSessionEnded requestId=" + sessionEndedRequest.requestId +
+//             ", sessionId=" + session.sessionId);
+//     // Add cleanup logic here
+// }
 
 // --------------- Functions that control the skill's behavior -----------------------
 
 
 
-function getWelcomeResponse(callback) {
-    // If we wanted to initialize the session to have some attributes we could add those here.
-    var sessionAttributes = {};
-    var cardTitle = "Welcome";
-    var speechOutput = "Welcome to Scheduler-Pro. Please say how many tasks you have for today";
-    // If the user either does not reply to the welcome message or says something that is not
-    // understood, they will be prompted again with this text.
-    var repromptText = "Please tell me what action to undertake. ";
-    var shouldEndSession = false;
+// function getWelcomeResponse(callback) {
+//     // If we wanted to initialize the session to have some attributes we could add those here.
+//     var sessionAttributes = {};
+//     var cardTitle = "Welcome";
+//     var speechOutput = "Welcome to Scheduler-Pro. Please say how many tasks you have for today";
+//     // If the user either does not reply to the welcome message or says something that is not
+//     // understood, they will be prompted again with this text.
+//     var repromptText = "Please tell me what action to undertake. ";
+//     var shouldEndSession = false;
 
-    callback(sessionAttributes,
-             buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
-}
-// --------------- Helpers that build all of the responses -----------------------
+//     callback(sessionAttributes,
+//              buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+// }
+// // --------------- Helpers that build all of the responses -----------------------
 
 function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
     return {
